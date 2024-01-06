b9fd95650fd980a5ea8dfbe604a5ebc7be894479
HarrtyXX <ddominik.kk123@wp.pl>
2024-01-06 02:04:21 +0100

    Add owner field to ToDoList model and __str__ function to models

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class ToDoList(models.Model):
     name = models.TextField(max_length=200)
-    owner = ...
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
 class Item(models.Model):
     list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.text
