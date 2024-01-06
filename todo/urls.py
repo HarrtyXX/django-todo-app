d6a5f8e5a754f0a22cdb431a731fca9f5f6cc740
HarrtyXX <ddominik.kk123@wp.pl>
2024-01-06 02:16:52 +0100

    Add feature to view list and their items at view/<id>

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('list/<int:id>', views.show_list, name="show_list")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import ToDoList
 
 # Create your views here.
 
 def home(request):
-    context = {}
-    return render(request, "todo/home.html", context)
+    return render(request, 'todo/home.html')
+
+
+def show_list(request, id):
+    list = ToDoList.objects.get(id=id)
+
+    context = {"list":list}
+    return render(request, 'todo/list.html', context)
