a1561fca2f5686200f4481fa91133d93bea6d0de
HarrtyXX <ddominik.kk123@wp.pl>
2024-01-06 01:49:40 +0100

    Add ToDoList and Item models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDoList(models.Model):
+    name = models.TextField(max_length=200)
+    owner = ...
+class Item(models.Model):
+    list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
